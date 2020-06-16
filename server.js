4b0c1e1d974630b4c021a5e8cacbaccc578d8396
Davi Mattos <davimattos@Davis-MacBook-Pro.local>
2020-06-15 22:32:18 -0300

    refactor port log

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const { createServer } = require('http')
-const next = require('next')
-const routes = require('./routes')
+const { createServer } = require('http');
+const next = require('next');
+const routes = require('./routes');
 
-const app = next({ dev: process.env.NODE_ENV !== 'production'})
+const app = next({ dev: process.env.NODE_ENV !== 'production' });
 const handler = routes.getRequestHandler(app);
 
 app.prepare().then(() => {
-    console.log('server is listening on port 30001');
-    createServer(handler).listen(3001);
-})
+  console.log('server is listening on port 3001');
+  createServer(handler).listen(3001);
+});
