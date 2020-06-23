68b420befbb306d0f44b609a248fe14b0b496d53
Davi Mattos <davimattos@Davis-MacBook-Pro.local>
2020-06-22 21:26:06 -0300

    create initial test

---
+++ __tests__/header.spec.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+import Header from 'components/header';
+
+it('should render Header', () => {
+  const { getByText } = render(<Header />);
+  expect(getByText('Header')).toBeInTheDocument();
+});
